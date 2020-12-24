0b184a75853c86acfab643f082c9529b079fe5c2
whis <whisdevj@gmail.com>
2020-12-24 23:35:23 +0900

    [config] add menus

---
+++ src/config/menus.js
@@ -0,0 +1,24 @@
+export const menus = {
+    listSee: [
+        {
+            label: '전체보기',
+            value: 'total'
+        },
+        {
+            label: '북마크보기',
+            value: 'bookmark'
+        }
+    
+    ],
+    currency: [
+        {
+            label: 'KRW 보기',
+            value: 'krw'
+        },
+        {
+            label: 'USD 보기',
+            value: 'usd'
+        }
+    ],
+    perPage: [10, 30, 50]
+}
