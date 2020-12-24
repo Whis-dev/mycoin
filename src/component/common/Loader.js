3fb643b5f4859bfc9d850e24e2e61406cd6aa18a
whis <whisdevj@gmail.com>
2020-12-24 23:34:04 +0900

    [common] add Loader

---
+++ src/component/common/Loader.js
@@ -0,0 +1,12 @@
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const Loader = () => {
+    return (
+        <div style={{ textAlign: 'center', width: '100%' }}>
+            <CircularProgress />
+        </div>
+    )
+
+}
+
+export default Loader;
